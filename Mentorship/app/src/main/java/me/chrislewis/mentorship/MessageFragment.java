38716a40a2427c2603d5d8e8eaa6930a5cdd8796
Christopher Lewis <cwlewis@fb.com>
2018-07-16 14:35:38 -0700

    Connects Bottom Navigation View with Message Fragment
    By clicking on the message button on the bottom navigation view
    the user brings up message fragment and user can upload message to
    Parse.

--- Mentorship/app/src/main/java/me/chrislewis/mentorship/MessageFragment.java
+++ Mentorship/app/src/main/java/me/chrislewis/mentorship/MessageFragment.java
@@ -4,9 +4,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class MessageFragment extends Fragment {
 
@@ -14,6 +22,12 @@ public class MessageFragment extends Fragment {
     public MessageFragment() {
         // Required empty public constructor
     }
+
+    static final String USER_ID_KEY = "userId";
+    static final String BODY_KEY = "body";
+
+    EditText etMessage;
+    Button bSend;
 
 
     @Override
@@ -25,6 +39,29 @@ public class MessageFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        etMessage = view.findViewById(R.id.etMessage);
+        bSend = view.findViewById(R.id.bSend);
+        bSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = etMessage.getText().toString();
+                ParseObject message = ParseObject.create("Message");
+                message.put(USER_ID_KEY, ParseUser.getCurrentUser().getObjectId());
+                message.put(BODY_KEY, data);
+                message.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null) {
+                            Log.d("Messages", "Working");
+                        }
+                        else {
+                            Log.d("Messages", "Fail");
+                        }
+                    }
+                });
+                etMessage.setText(null);
+            }
+        });
     }
+
 }
