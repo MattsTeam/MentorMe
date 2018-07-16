8f0e19e5b123895e4e47eebe752c6fcfd2aa9180
Vanessa Yan <vanessahlyan@fb.com>
2018-07-16 14:03:52 -0700

    with home fragment

--- Mentorship/app/src/main/java/me/chrislewis/mentorship/MainActivity.java
+++ Mentorship/app/src/main/java/me/chrislewis/mentorship/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -22,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    //mTextMessage.setText(R.string.title_home);
+                    //fragmentTransaction
                     return true;
                 case R.id.navigation_favorites:
                     mTextMessage.setText(R.string.title_dashboard);
@@ -49,6 +51,9 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.flContainer, homeFragment).commit();
 
 
     }
