4930fc54c69a98d0430488bfa5a714b34ea6fc06
Weyxin Ly <weyxin99@fb.com>
2018-07-17 16:09:15 -0700

    calendar's today date is colored

--- Mentorship/app/src/main/java/me/chrislewis/mentorship/CalendarFragment.java
+++ Mentorship/app/src/main/java/me/chrislewis/mentorship/CalendarFragment.java
@@ -46,24 +46,36 @@ public class CalendarFragment extends Fragment implements OnDateSelectedListener
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         Collection<CalendarDay> days = new ArrayList<>();
+        Collection<CalendarDay> todayList = new ArrayList<>();
         calendarView = view.findViewById(R.id.calendarView);
         todayText = view.findViewById(R.id.tvCurrentDay);
         calendar = Calendar.getInstance();
         calendarView.setDateSelected(calendar.getTime(), true);
-        String todayString = dateFormat.format(calendar.getTime());
-        Log.d("CalendarFragment", todayString);
-        todayText.setText(todayString);
+        todayText.setText(dateFormat.format(calendar.getTime()));
+        Log.d("CalendarFragment", dateFormat.format(calendar.getTime()));
         CalendarDay day1 = CalendarDay.from(2018, 5, 25);
         CalendarDay day2 = CalendarDay.from(2018, 6, 30);
+        CalendarDay today = CalendarDay.from(calendar.getTime());
         days.add(day1);
         days.add(day2);
+        todayList.add(today);
         calendarView.setOnDateChangedListener(this);
-        calendarView.addDecorators(new CurrentDayDecorator(R.color.loginGreen, days));
-        //calendarView.addDecorators(new CurrentDayDecorator(getActivity(), days));
+        int eventGreen = getIntFromColor(15, 164, 124);
+        int todayGreen = getIntFromColor(54, 55, 60);
+        calendarView.addDecorators(new CurrentDayDecorator(todayGreen, todayList));
+        calendarView.addDecorators(new CurrentDayDecorator(eventGreen, days));
     }
 
     @Override
     public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay calendarDay, boolean b) {
         Log.d("CalendarFragment", "Selected something");
+    }
+
+    public int getIntFromColor(int Red, int Green, int Blue){
+        Red = (Red << 16) & 0x00FF0000;
+        Green = (Green << 8) & 0x0000FF00;
+        Blue = Blue & 0x000000FF;
+
+        return 0xFF000000 | Red | Green | Blue;
     }
 }
