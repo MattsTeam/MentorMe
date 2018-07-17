b9a8f1b54f0695aee2e7684b4697f608c9a6508b
Weyxin Ly <weyxin99@fb.com>
2018-07-17 13:41:54 -0700

    fragment calendar xml

--- Mentorship/app/src/main/java/me/chrislewis/mentorship/CalendarFragment.java
+++ Mentorship/app/src/main/java/me/chrislewis/mentorship/CalendarFragment.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
@@ -15,6 +16,7 @@ import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 
@@ -22,8 +24,10 @@ import me.chrislewis.mentorship.models.CurrentDayDecorator;
 
 public class CalendarFragment extends Fragment implements OnDateSelectedListener{
 
-    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd, yyyy");
     MaterialCalendarView calendarView;
+    Calendar calendar;
+    TextView todayText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
@@ -42,12 +46,17 @@ public class CalendarFragment extends Fragment implements OnDateSelectedListener
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         Collection<CalendarDay> days = new ArrayList<>();
+        calendarView = view.findViewById(R.id.calendarView);
+        todayText = view.findViewById(R.id.tvCurrentDay);
+        calendar = Calendar.getInstance();
+        calendarView.setDateSelected(calendar.getTime(), true);
+        String todayString = dateFormat.format(calendar.getTime());
+        Log.d("CalendarFragment", todayString);
+        todayText.setText(todayString);
         CalendarDay day1 = CalendarDay.from(2018, 5, 25);
-        CalendarDay day2 = CalendarDay.from(2018, 5, 30);
+        CalendarDay day2 = CalendarDay.from(2018, 6, 30);
         days.add(day1);
         days.add(day2);
-        calendarView = view.findViewById(R.id.calendarView);
-        calendarView.setCurrentDate(CalendarDay.from(2018, 6, 17), true);
         calendarView.setOnDateChangedListener(this);
         calendarView.addDecorators(new CurrentDayDecorator(R.color.loginGreen, days));
         //calendarView.addDecorators(new CurrentDayDecorator(getActivity(), days));
