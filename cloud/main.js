89a40d79e11ea754b8f4c4b4f3b673aa89d17a28
Vanessa Yan <vanessahlyan@fb.com>
2018-07-21 00:34:44 -0700

    add custom receiver, update json files

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,29 @@
+Parse.Cloud.define('pingReply', function(request, response) {
+  var params = request.params;
+  var customData = params.customData;
+  var deviceToken = request.params.deviceToken;
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+  if (!customData) {
+    response.error("Missing customData!")
+  }
+
+  var sender = JSON.parse(customData).sender;
+  var query = new Parse.Query(Parse.Installation);
+  query.equalTo("installationId", sender);
+  query.equalTo("deviceType", "android");
+
+
+  Parse.Push.send({
+  where: query,
+  // Parse.Push requires a dictionary, not a string.
+  data: {"alert": "The Giants scored!"},
+  }, {
+  useMasterKey: true,
+  success: function() {
+     console.log("#### PUSH OK");
+  }, error: function(error) {
+     console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true});
+
+  response.success('success');
 });
