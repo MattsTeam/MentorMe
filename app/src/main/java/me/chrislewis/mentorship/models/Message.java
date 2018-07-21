7cd64ab864b39051b4888004cde92f5ef71c13cc
Christopher Lewis <cwlewis@fb.com>
2018-07-20 17:10:00 -0700

    Add Message Query Subclass

--- app/src/main/java/me/chrislewis/mentorship/models/Message.java
+++ app/src/main/java/me/chrislewis/mentorship/models/Message.java
@@ -2,11 +2,14 @@ package me.chrislewis.mentorship.models;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 @ParseClassName("Message")
 public class Message extends ParseObject{
+    private final static String USER_KEY = "user";
     private final static String USER_ID_KEY = "userId";
     private final static String BODY_KEY = "body";
+    private final static String CREATED_AT_KEY = "createdAt";
 
 
     public String getBody() {
@@ -23,5 +26,20 @@ public class Message extends ParseObject{
 
     public void setUserIdKey(String userId) {
         put(USER_ID_KEY, userId);
+    }
+
+    public static class Query extends ParseQuery<Message> {
+        public Query() {
+            super(Message.class);
+        }
+        public Query getTop() {
+            orderByDescending(CREATED_AT_KEY);
+            setLimit(50);
+            return this;
+        }
+        public Query withUser() {
+            include(USER_KEY);
+            return this;
+        }
     }
 }
