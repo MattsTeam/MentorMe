b01e2738592c8fed39b8247104bff1afdcad5b95
Vanessa Yan <vanessahlyan@fb.com>
2018-08-02 18:22:10 -0700

    Merge Favorites Adapter

--- app/src/main/java/me/chrislewis/mentorship/FavoritesAdapter.java
+++ app/src/main/java/me/chrislewis/mentorship/FavoritesAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
