9abfb209c3a7adcfd6af95e8d816dd1ae2e23eee
Vanessa Yan <vanessahlyan@fb.com>
2018-08-06 14:36:03 -0700

    Add drawables

--- app/src/main/java/me/chrislewis/mentorship/ProfileCategoriesFragment.java
+++ app/src/main/java/me/chrislewis/mentorship/ProfileCategoriesFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class ProfileCategoriesFragment extends Fragment {
 
@@ -18,6 +21,37 @@ public class ProfileCategoriesFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        private ImageButton languagesButton;
+        private ImageView ivLanguages;
+        private ImageButton artButton;
+        private ImageView ivArt;
+        private ImageButton engineeringButton;
+        private ImageView ivEngineering;
+        private ImageButton sportsButton;
+        private ImageView ivSports;
+        private ImageButton humanitiesButton;
+        private ImageView ivHumanities;
+        private ImageView sciencesButton;
+        private ImageView ivSciences;
+        private Button mentorButton;
+        private Button menteeButton;
+        private Button changeButton;
+        private String username;
+        private String password;
 
+        engineeringButton = view.findViewById(R.id.engineeringButton);
+        ivEngineering = view.findViewById(R.id.ivEngineering);
+        artButton = view.findViewById(R.id.artButton);
+        ivArt = view.findViewById(R.id.ivArt);
+        sportsButton = view.findViewById(R.id.sportsButton);
+        ivSports = view.findViewById(R.id.ivSport);
+        sciencesButton = view.findViewById(R.id.scienceButton);
+        ivSciences = view.findViewById(R.id.ivSciences);
+        humanitiesButton = view.findViewById(R.id.humanitiesButton);
+        ivHumanities = view.findViewById(R.id.ivHumanities);
+        languagesButton = view.findViewById(R.id.languagesButton);
+        ivLanguages = view.findViewById(R.id.ivLanguages);
+        mentorButton = view.findViewById(R.id.mentorButton);
+        menteeButton = view.findViewById(R.id.menteeButton);
     }
 }
