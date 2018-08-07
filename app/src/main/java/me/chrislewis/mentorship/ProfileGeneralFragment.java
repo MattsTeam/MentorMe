0cf1a2c392aa536913ddef86bd56a1077f0e5712
Weyxin Ly <weyxin99@fb.com>
2018-08-07 12:32:43 -0700

    Fix profile fragment crash

--- app/src/main/java/me/chrislewis/mentorship/ProfileGeneralFragment.java
+++ app/src/main/java/me/chrislewis/mentorship/ProfileGeneralFragment.java
@@ -5,20 +5,31 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.flexbox.FlexDirection;
+import com.google.android.flexbox.FlexWrap;
+import com.google.android.flexbox.FlexboxLayoutManager;
+import com.google.android.flexbox.JustifyContent;
+
+import java.util.List;
+
 import me.chrislewis.mentorship.models.User;
 
 public class ProfileGeneralFragment extends Fragment {
-    TextView tvSkills;
     TextView tvSummary;
     TextView tvEdu;
     TextView tvDescription;
+    TextView tvOrganization;
     SharedViewModel model;
     User user;
+
+    RecyclerView rvSkills;
+    SkillsAdapter skillsAdapter;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -32,24 +43,31 @@ public class ProfileGeneralFragment extends Fragment {
         user = model.getCurrentUser();
 
         tvEdu = view.findViewById(R.id.tvEdu);
-        tvSkills = view.findViewById(R.id.tvSkills);
-        tvSummary = view.findViewById(R.id.tvSummary);
+        rvSkills = view.findViewById(R.id.rvSkills);
         tvDescription = view.findViewById(R.id.tvDescription);
+        tvOrganization = view.findViewById(R.id.tvOrganization);
         populateInfo();
     }
 
     private void populateInfo() {
-        if (user.getSkills() != null) {
-            tvSkills.setText(user.getSkills());
-        }
-        if (user.getSummary() != null) {
-            tvSummary.setText(user.getSummary());
+        List<String> skills = user.getSkillsList();
+        if(skills != null) {
+            skillsAdapter = new SkillsAdapter(skills);
+            FlexboxLayoutManager flexboxLayoutManager = new FlexboxLayoutManager(getContext());
+            flexboxLayoutManager.setFlexDirection(FlexDirection.ROW);
+            flexboxLayoutManager.setFlexWrap(FlexWrap.WRAP);
+            flexboxLayoutManager.setJustifyContent(JustifyContent.FLEX_START);
+            rvSkills.setLayoutManager(flexboxLayoutManager);
+            rvSkills.setAdapter(skillsAdapter);
         }
         if (user.getEducation() != null) {
             tvEdu.setText(user.getEducation());
         }
         if (user.getDescription() != null) {
             tvDescription.setText(user.getDescription());
+        }
+        if (user.getOrganization() != null) {
+            tvOrganization.setText(user.getOrganization());
         }
     }
 }
