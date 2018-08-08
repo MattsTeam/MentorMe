43bb6e9e1d5f25550cc506bbafefe3fd572c7989
Weyxin Ly <weyxin99@fb.com>
2018-08-08 12:43:28 -0700

    Add slide animation when back button on details fragment is pressed

--- app/src/main/java/me/chrislewis/mentorship/DetailsFragment2.java
+++ app/src/main/java/me/chrislewis/mentorship/DetailsFragment2.java
@@ -8,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -112,7 +113,9 @@ public class DetailsFragment2 extends Fragment {
             @Override
             public void onClick(View view) {
                 SearchFragment searchFragment = new SearchFragment();
-                model.getFragmentManager().beginTransaction().replace(R.id.flContainer, searchFragment).commit();
+                FragmentTransaction transaction = model.getFragmentManager().beginTransaction();
+                transaction.setCustomAnimations(R.anim.anim_slide_out_right, R.anim.anim_slide_in_right, R.anim.anim_slide_out_right, R.anim.anim_slide_in_right);
+                transaction.replace(R.id.flContainer, searchFragment).commit();
             }
         });
 
